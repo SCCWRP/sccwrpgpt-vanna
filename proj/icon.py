ed99f5c46467d72423522dda05fe064992bb7f32
Robert Butler or someone else at SCCWRP <robertb@sccwrp.org>
2024-03-12 22:39:38 +0000

    fixed path to icon

--- proj/icon.py
+++ proj/icon.py
@@ -1,15 +1,14 @@
 import os
-from flask import Blueprint, make_response
+from flask import Blueprint, make_response, url_for
 from .utils.vanna import vn
 
 icon_bp = Blueprint('icon_bp', __name__)
 
 @icon_bp.route('/icon')
 def send_icon():
-    #iconpath = os.path.join( os.getcwd() , 'static/vanna.svg' ) ## not working incorrect getcwd
-    iconpath = os.path.join( '/home/app/proj/' , 'static/vanna.svg' ) ## not working incorrect getcwd
-    #/home/app/static/vanna.svg
-    #/home/app/proj/static/vanna.svg
+    
+    iconpath = os.path.join(os.getcwd(), 'proj', 'static', 'vanna.svg')
+    
     with open(iconpath, 'r') as iconfile:
         iconbinary = iconfile.read()
 
